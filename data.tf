dae8875c7a649de78fc6a4f218c10e57e684a934
hemant96c <132544701+hemant96c@users.noreply.github.com>
2023-05-16 18:46:31 +0530

    Update data.tf

--- data.tf
+++ data.tf
@@ -1,11 +1,17 @@
 data "aws_nat_gateway" "nat" {
-  id = "nat-089afc4a1054d134c"
+  id = "nat-0e2e0998357554998"
 }
 
 data "aws_vpc" "vpc" {
-  id = "vpc-00bf0d10a6a41600c"
+  id = "vpc-09148ac6c32ec29f5"
 }
 
 data "aws_iam_role" "lambda" {
-  name = "DevOps-Candidate-Lambda-Role"
+  name = "MyIAMRole"
 }
+
+data "aws_subnet" "subnet" {
+  id = "subnet-0a8775d07f70b9c3f"
+}
+
+
